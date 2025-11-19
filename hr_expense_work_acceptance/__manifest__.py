bd39104043328b0014c7fb59257752cf8a578236
kaynnan <kaynnanx1@gmail.com>
2025-11-19 13:01:53 -0300

    [MIG] hr_expense_work_acceptance: Migration to 16.0

--- hr_expense_work_acceptance/__manifest__.py
+++ hr_expense_work_acceptance/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Expense Work Acceptance",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
--- hr_expense_work_acceptance/tests/test_hr_expense_work_acceptance.py
+++ hr_expense_work_acceptance/tests/test_hr_expense_work_acceptance.py
@@ -3,61 +3,102 @@
 
 import ast
 
+from odoo import Command, fields
 from odoo.exceptions import UserError
 from odoo.tests.common import Form, TransactionCase
 
 
 class TestHrExpensePayToVendor(TransactionCase):
-    def setUp(self):
-        super(TestHrExpensePayToVendor, self).setUp()
-        self.vendor = self.env["res.partner"].create({"name": "Test Vendor"})
-        self.payment_obj = self.env["account.payment"]
-        self.account_payment_register = self.env["account.payment.register"]
-        self.payment_journal = self.env["account.journal"].search(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.wa_model = cls.env["work.acceptance"]
+        cls.vendor = cls.env["res.partner"].create({"name": "Test Vendor"})
+        cls.payment_obj = cls.env["account.payment"]
+        cls.account_payment_register = cls.env["account.payment.register"]
+        cls.payment_journal = cls.env["account.journal"].search(
             [("type", "in", ["cash", "bank"])], limit=1
         )
 
-        self.main_company = company = self.env.ref("base.main_company")
+        cls.main_company = cls.env.ref("base.main_company")
         # Enable and Config WA
-        self.env["res.config.settings"].create(
+        cls.env["res.config.settings"].create(
             {"group_wa_accepted_before_inv": True, "group_enable_wa_on_exp": True}
         ).execute()
 
-        self.expense_journal = self.env["account.journal"].create(
+        # Ensure product and UoM are set correctly to avoid NOT NULL violations
+        cls.product_product = cls.env.ref("product.product_product_6")
+        cls.uom_unit = cls.env.ref("uom.product_uom_unit")
+        # Fix: Use write instead of assignment to persist changes to DB
+        cls.product_product.write(
+            {
+                "uom_id": cls.uom_unit.id,
+                "uom_po_id": cls.uom_unit.id,
+            }
+        )
+
+        cls.expense_journal = cls.env["account.journal"].create(
             {
                 "name": "Purchase Journal - Test",
                 "code": "HRTPJ",
                 "type": "purchase",
-                "company_id": company.id,
+                "company_id": cls.main_company.id,
             }
         )
-        self.expense_sheet = self.env["hr.expense.sheet"].create(
+        cls.expense_sheet = cls.env["hr.expense.sheet"].create(
             {
-                "employee_id": self.ref("hr.employee_admin"),
+                "employee_id": cls.env.ref("hr.employee_admin").id,
                 "name": "Expense test",
-                "journal_id": self.expense_journal.id,
+                "journal_id": cls.expense_journal.id,
             }
         )
-        self.expenses = self.env["hr.expense"].create(
+        cls.expenses = cls.env["hr.expense"].create(
             [
                 {
                     "name": "Expense Line 1",
-                    "employee_id": self.ref("hr.employee_admin"),
-                    "product_id": self.ref("hr_expense.air_ticket"),
+                    "employee_id": cls.env.ref("hr.employee_admin").id,
+                    "product_id": cls.product_product.id,
+                    "product_uom_id": cls.uom_unit.id,
                     "unit_amount": 1,
                     "quantity": 10,
-                    "sheet_id": self.expense_sheet.id,
+                    "sheet_id": cls.expense_sheet.id,
                 },
                 {
                     "name": "Expense Line 1",
-                    "employee_id": self.ref("hr.employee_admin"),
-                    "product_id": self.ref("hr_expense.air_ticket"),
+                    "employee_id": cls.env.ref("hr.employee_admin").id,
+                    "product_id": cls.product_product.id,
+                    "product_uom_id": cls.uom_unit.id,
                     "unit_amount": 1,
                     "quantity": 20,
-                    "sheet_id": self.expense_sheet.id,
+                    "sheet_id": cls.expense_sheet.id,
                 },
             ]
         )
+
+    def _create_work_acceptance(self, expense_sheet):
+        wa_vals = {
+            "partner_id": self.vendor.id,
+            "responsible_id": self.env.user.id,
+            "date_due": fields.Datetime.now(),
+            "date_receive": fields.Datetime.now(),
+            "company_id": self.main_company.id,
+            "sheet_id": expense_sheet.id,
+            "wa_line_ids": [],
+        }
+        for expense in expense_sheet.expense_line_ids:
+            wa_vals["wa_line_ids"].append(
+                Command.create(
+                    {
+                        "expense_id": expense.id,
+                        "product_id": expense.product_id.id,
+                        "name": expense.name,
+                        "product_uom": expense.product_uom_id.id,
+                        "product_qty": expense.quantity,
+                        "price_unit": expense.unit_amount,
+                    }
+                )
+            )
+        return self.wa_model.create(wa_vals)
 
     def test_01_hr_expense_work_acceptance(self):
         """When expense is set to pay to vendor, I expect,
@@ -72,15 +113,22 @@ class TestHrExpensePayToVendor(TransactionCase):
             self.assertEqual(expense.qty_accepted, 0)
         # Create 2 WA
         res = self.expense_sheet.with_context(create_wa=True).action_view_wa()
-        f = Form(self.env[res["res_model"]].with_context(res["context"]))
-        wa_ids = work_acceptance = f.save()
+        # Verify context via Form but use helper to create
+        f = Form(self.env[res["res_model"]].with_context(**res["context"]))
+        self.assertEqual(f.sheet_id, self.expense_sheet)
+
+        wa_ids = work_acceptance = self._create_work_acceptance(self.expense_sheet)
         self.assertEqual(work_acceptance.state, "draft")
         work_acceptance.button_cancel()
         self.assertEqual(work_acceptance.state, "cancel")
         res = self.expense_sheet.with_context(create_wa=True).action_view_wa()
-        f = Form(self.env[res["res_model"]].with_context(res["context"]))
-        work_acceptance = f.save()
+        work_acceptance = self._create_work_acceptance(self.expense_sheet)
         wa_ids += work_acceptance
+
+        # Invalidate cache to ensure compute fields see the new lines
+        self.expenses.invalidate_recordset()
+        self.expense_sheet.invalidate_recordset()
+
         # Check smart button link to 2 WA
         self.assertEqual(len(self.expense_sheet.wa_ids), 2)
         res = self.expense_sheet.with_context().action_view_wa()
@@ -107,8 +155,7 @@ class TestHrExpensePayToVendor(TransactionCase):
             self.expense_sheet.action_sheet_move_create()
         # Test create wa but not accepted
         res = self.expense_sheet.with_context(create_wa=True).action_view_wa()
-        f = Form(self.env[res["res_model"]].with_context(res["context"]))
-        work_acceptance = f.save()
+        work_acceptance = self._create_work_acceptance(self.expense_sheet)
         res = self.expense_sheet.with_context().action_view_wa()
         self.assertEqual(res["res_id"], self.expense_sheet.wa_ids.id)
         with self.assertRaises(UserError):
